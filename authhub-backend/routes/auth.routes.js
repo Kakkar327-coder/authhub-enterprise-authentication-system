da8c20e99953e6fa0f9cc143e7cb9e5becac7120
Kavya Kakkar <Kavyakakkar327@gmail.com>
2026-06-13 09:41:58 +0530

    feat: protect dashboard route using jwt middleware

--- authhub-backend/routes/auth.routes.js
+++ authhub-backend/routes/auth.routes.js
@@ -3,7 +3,37 @@ const express =require("express");
 const router =express.Router();
 
 const {signup}=require("../controllers/auth.controller");
+const {login}=require("../controllers/auth.controller");
+const {refreshToken}=require("../controllers/auth.controller");
+const authMiddleware =require("../middleware/auth.middleware");
 
-router.post("/signup",signup);
+
+router.post(
+ "/signup",
+ signup
+);
+
+router.post(
+ "/login",
+ login
+);
+
+router.post(
+ "/refresh-token",
+ refreshToken
+);
+
+router.get(
+ "/dashboard",
+ authMiddleware,
+ (req,res)=>{
+
+  res.json({
+   message:
+   "Welcome",
+   user:req.user
+  });
+
+ });
 
 module.exports =router;
