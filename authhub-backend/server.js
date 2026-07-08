692e2427589b4381bc4deb2a4e1e0c60633f47f9
Kavya Kakkar <Kavyakakkar327@gmail.com>
2026-07-08 09:27:56 +0530

    Implement backend updates and complete frontend development

--- authhub-backend/server.js
+++ authhub-backend/server.js
@@ -1,12 +1,24 @@
-require("dotenv").config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') })
+const express=require('express')
+const mongoose=require('mongoose')
+const router = require('./routes/user.routes')
+const cors=require('cors')
+const app=express()
+app.use(express.json())
+app.use(cors())
 
-const app =require("./app");
+async function connectdb(){
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("connection is done")
+    } catch (err) {
+        console.error("MongoDB Connection Error: Please verify that your current IP address is whitelisted in your MongoDB Atlas Network Access configuration.")
+        console.error("Error details:", err.message)
+    }
+}
+connectdb()
 
-const connectDB =require("./config/db");
-
-connectDB();
-
-app.listen(process.env.PORT,()=>{
-   console.log(`Server Running`);
- }
-);
+app.use("/api/v1/data",router)
+console.log("MAIL =", process.env.MAIL);
+console.log("PASSI =", process.env.PASSI);
+app.listen(process.env.PORT)
